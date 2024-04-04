f815be9b0a4170ad01894bbc7b50f8c560d44178
eduard <eduardcamayo13@gmail.com>
2024-04-04 09:50:23 -0500

    Teminando el componente Hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,40 +1,43 @@
 import { basics } from "@cv";
 import Section from "../Section";
-import Mail from "@/icons/Mail";
-import Phone from "@/icons/Phone";
-import GitHub from "@/icons/GitHub";
-import X from "@/icons/X";
-import LinkedIn from "@/icons/Linkedin";
 import WorldMap from "@/icons/WorldMap";
+import { CiMail, CiGlobe  } from "react-icons/ci";
+import { HiOutlinePhone } from "react-icons/hi";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Hero = () => {
     const { name, label, image, location, profiles, phone, email } = basics;
     const { city, region } = location;
 
     const SOCIALICONS: any = {
-        GitHub,
-        LinkedIn,
-        X,
+        GitHub: FaGithub,
+        LinkedIn: FaLinkedin,
+        X: FaXTwitter,
     };
     return (
         <Section>
             <div className="flex items-center justify-between gap-4">
-                <div className="">
-                    <h1 className="text-3xl font-medium">{name}</h1>
-                    <h2 className="text-[#444] font-medium text-balance">{label}</h2>
-                    <span className="flex gap-3">
-                        <WorldMap/>
+                <div className="flex flex-col gap-1 pr-8">
+                    <h1 className="text-2xl font-medium">{name}</h1>
+                    <h2 className="text-[#444] font-medium text-balance">
+                        {label}
+                    </h2>
+                    <span className="flex items-center gap-2 font-mono text-[#444]">
+                        <CiGlobe className="w-[18px] h-[18px]" />
                         {city}, {region}
                     </span>
-                    <footer className="flex gap-3">
+                    <footer className="flex gap-1 mt-2">
                         {email && (
                             <a
                                 title={`Enviar un correo electrónico a ${name} al correo ${email}`}
                                 href={`mailto:${email}`}
                                 target="_blank"
                                 rel="noopener noreferrer"
+                                className="w-[32px] h-[32px] flex items-center justify-center text-[#777] p-1 border rounded-md transition-all duration-200 ease-linear border-[#eee] hover:bg-[#eee]"
                             >
-                                <Mail />
+                                <CiMail className="w-[18px] h-[18px] " />
                             </a>
                         )}
                         {phone && (
@@ -43,8 +46,9 @@ const Hero = () => {
                                 href={`tel:${phone}`}
                                 target="_blank"
                                 rel="noopener noreferrer"
+                                className="w-[32px] h-[32px] flex items-center justify-center text-[#777] p-1 border rounded-md transition-all duration-200 ease-linear border-[#eee] hover:bg-[#eee]"
                             >
-                                <Phone />
+                                <HiOutlinePhone className="w-[18px] h-[18px]" />
                             </a>
                         )}
                         {profiles.map(({ network, url, username }, index) => {
@@ -56,16 +60,21 @@ const Hero = () => {
                                     title={`Visitar el perfil de ${name} en ${network}`}
                                     href={url}
                                     target="_blank"
+                                    className="w-[32px] h-[32px] flex items-center justify-center text-[#777] p-1 border rounded-md transition-all duration-200 ease-linear border-[#eee] hover:bg-[#eee] hover:border-[#ddd]"
                                     rel="noopener noreferrer"
                                 >
-                                    <Icon />
+                                    <Icon className="w-[18px] h-[18px]" />
                                 </a>
                             );
                         })}
                     </footer>
                 </div>
                 <figure>
-                     <img className="aspect-square object-cover w-[128px] rounded-2xl" src={image} alt={name} />
+                    <img
+                        className="aspect-square object-cover w-[128px] rounded-2xl"
+                        src={image}
+                        alt={name}
+                    />
                 </figure>
             </div>
         </Section>
