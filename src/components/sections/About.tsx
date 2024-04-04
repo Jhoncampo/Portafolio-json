ff2c85fea8a0f215d5ac45da8aadaf439bf6cdc7
eduard <eduardcamayo13@gmail.com>
2024-04-04 09:56:16 -0500

    Componente About teminado

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Section from '@/components/Section'
+import {basics} from "@cv"
 
 const About = () => {
+  const {summary} = basics
   return (
     <Section title='About'>
-
+      <p className='text-[#666] text-sm text-wrap m-0'>{summary}</p>
     </Section>
   )
 }
