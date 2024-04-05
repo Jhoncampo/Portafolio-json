799610108afa7ddcb6c994d5c00f90848c700603
Jhon Eduard <eduardcamayo13@gmail.com>
2024-04-04 20:51:45 -0500

    Modificando la fuente y el tamaño

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,13 +1,13 @@
-import Section from '@/components/Section'
-import {basics} from "@cv"
+import Section from "@/components/Section";
+import { basics } from "@cv";
 
 const About = () => {
-  const {summary} = basics
-  return (
-    <Section title='About'>
-      <p className='text-[#666] text-sm text-wrap m-0'>{summary}</p>
-    </Section>
-  )
-}
+    const { summary } = basics;
+    return (
+        <Section title="About">
+            <p className="text-[#666] font-mono text-wrap m-0">{summary}</p>
+        </Section>
+    );
+};
 
-export default About
+export default About;
